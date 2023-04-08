70c3f28b6592c1c7a65614fb9fed95cf4d722a72
Aradhya Tiwari <aradhya.prad@gmail.com>
2023-04-08 16:45:31 +0530

    product comp started and vite hmr debuged for wsl

---
+++ src/lib/components/Products.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let name;
+    export let rating;
+    export let desc;
+    export let price;
+    export let img;
+</script>
+
+<main class="w-[100%] h-[800px] flex flex-wrap">
+    <div class="w-[30%] h-[80%] border border-black m-auto rounded-lg p-10">
+        <div>
+            <img src={img} alt="" srcset="" class="w-full rounded-lg border" />
+        </div>
+    </div>
+</main>
