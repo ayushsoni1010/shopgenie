05b58e79f25512570fe02654c380a9a435ed6b24
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-04-08 20:09:09 +0530

    feat: landing

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -28,21 +28,50 @@
   <header>
     <div class="flex items-center justify-between">
       <a href="/" class="">
-        <div>
-          <img src="images/logo.png" alt="Logo" class="" srcset="" />
-          <p>Shop Genie</p>
+        <div class="flex items-center relative top-2">
+          <img
+            src="/images/logo.png"
+            alt="Logo"
+            class="w-24 rounded-full"
+            srcset=""
+          />
         </div>
       </a>
-      <div class="flex items-center list-none gap-3">
+      <div class="flex items-center list-none text-white">
         {#each navbarDetails as { item, url }, i}
-          <li>
+          <li class="hover:bg-gray-700 px-4 py-2 rounded-md drop-shadow">
             <a target="_blank" href={url} rel="noreferrer">
               {item}
             </a>
           </li>
         {/each}
       </div>
-      <div />
+      <div class="flex items-center gap-2">
+        <a
+          href="/login"
+          class="px-4 py-2 border-2 border-gray-100 rounded-md text-white"
+          >Login</a
+        >
+        <a href="/signup" class="px-4 py-2 bg- bg-gray-100 rounded-md">Signup</a
+        >
+        <!-- <a
+          href="/new"
+          class="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
+        >
+          <svg
+            width="20"
+            height="20"
+            fill="currentColor"
+            class="mr-2"
+            aria-hidden="true"
+          >
+            <path
+              d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z"
+            />
+          </svg>
+          New
+        </a> -->
+      </div>
     </div>
   </header>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,29 @@
   import Header from "../lib/components/Header.svelte";
 </script>
 
-<Header />
+<div
+  class="bg-gradient-to-r from-gray-700 via-gray-900 to-black h-screen px-20"
+>
+  <Header />
+  <div class="mt-20 grid grid-cols-2 grid-rows-2">
+    <div class="mt-10 flex items-start justify-around flex-col">
+      <h1 class="text-5xl text-white leading-tight text-left font-bold">
+        Shop till you drop without leaving the comfort of your laptop's top!
+      </h1>
+      <p class="text-white text-lg">
+        Experience shopping like never before. Our ecommerce store offers
+        augmented reality and voice-enabled search, making it accessible to all,
+        including our valued disabled customers
+      </p>
+      <a
+        href="/products"
+        class="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-lg font-medium p-3 px-6 shadow-sm"
+      >
+        Explore products
+      </a>
+    </div>
+    <div>
+      <video src="/images/animated.mp4" class="rounded-xl" />
+    </div>
+  </div>
+</div>
