e0a176945d6c64452e01c11f5f56a3d5a104aa7b
AZU-URE <202051069@iiitvadodara.ac.in>
2023-04-09 03:35:02 +0530

    added features component

---
+++ src/lib/components/Features.svelte
@@ -0,0 +1,68 @@
+<div
+  class="pr-[200px] pl-[200px] pt-[100px] w-[100%] bg-gray-900 flex flex-row
+  items-center">
+  <div class="m-0 p-0">
+    <img
+      alt="voice to text"
+      class="w-[100%] h-[100%]"
+      src="images/Speech-to-text-pana.png" />
+  </div>
+
+  <div class="flex flex-col text-left space-y-4">
+    <div>
+      <h1 class="text-white text-[30px] font-bold">Voice Assistant</h1>
+    </div>
+    <div>
+
+      <p class="text-gray-500 text-[18px]">
+        Customers can use voice commands to search for products on the website,
+        which can save time and effort compared to typing in a search query. The
+        voice assistant can also provide personalized product recommendations
+        based on the user's browsing and purchase history.
+      </p>
+    </div>
+    <div>
+      <a
+        href="#"
+        class="text-blue-400 hover:bg-gray-800 p-1.5 rounded-md font-semibold
+        underline-offset-4">
+        Try It Now &#x2192
+      </a>
+    </div>
+  </div>
+</div>
+
+<!-- 2nd feature -->
+
+<div
+  class="pr-[200px] pl-[200px] pt-[100px] w-[100%] bg-gray-900 pb-[100px] flex
+  flex-row-reverse items-center">
+  <div class="m-0 p-0">
+    <img
+      alt="voice to text"
+      class="w-[100%] h-[100%]"
+      src="images/3d-modeling.png" />
+  </div>
+
+  <div class="flex flex-col text-left space-y-4">
+    <div>
+      <h1 class="text-white text-[30px] font-bold">Product Visulization</h1>
+    </div>
+    <div>
+      <p class="text-gray-500 text-[18px]">
+        AR can also be used to help customers visualize how a product would look
+        in their home or space. For example, furniture retailers can use AR to
+        show customers how a particular piece of furniture would look in their
+        living room.
+      </p>
+    </div>
+    <div>
+      <a
+        href="#"
+        class="text-blue-400 hover:bg-gray-800 p-1.5 rounded-md font-semibold
+        underline-offset-4">
+        Try it now &#x2192
+      </a>
+    </div>
+  </div>
+</div>
