1743bf11d663a831089dab9931b135dad8b5ef16
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-04-08 17:07:26 +0530

    refactor: major refactored code changes

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,48 @@
+<script>
+  export const ssr = false;
+  let navbarDetails = [
+    {
+      item: "Home",
+      url: "/",
+    },
+    {
+      item: "About",
+      url: "/about",
+    },
+    {
+      item: "Products",
+      url: "/products",
+    },
+    {
+      item: "Collections",
+      url: "/collections",
+    },
+    {
+      item: "Contact",
+      url: "/contact",
+    },
+  ];
+</script>
+
+<div>
+  <header>
+    <div class="flex items-center justify-between">
+      <a href="/" class="">
+        <div>
+          <img src="images/logo.png" alt="Logo" class="" srcset="" />
+          <p>Shop Genie</p>
+        </div>
+      </a>
+      <div class="flex items-center list-none gap-3">
+        {#each navbarDetails as { item, url }, i}
+          <li>
+            <a target="_blank" href={url} rel="noreferrer">
+              {item}
+            </a>
+          </li>
+        {/each}
+      </div>
+      <div />
+    </div>
+  </header>
+</div>
